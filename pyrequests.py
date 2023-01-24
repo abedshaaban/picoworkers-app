4a4ffeadf89139249a5c16a865f98bfe5bb68e7c
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-01-24 15:51:22 +0200

    Delete pyrequests.py

--- pyrequests.py
+++
@@ -1,9 +0,0 @@
-# import json
-
-# def jsonFormat(obj):
-#     # create a formatted string of the Python JSON object
-#     text = json.dumps(obj, sort_keys=True, indent=4)
-#     print(text)
-
-
-# sample_string = '71a57b55'+':'+'82bQy9Eart27pnSj9xtcdNKTgC17ExQs'
